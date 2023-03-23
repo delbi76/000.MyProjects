7eea1804a137c11cc209403cebdce6079bf7f561
Apostolos Delbizis <apostolos.delbizis@gmail.com>
2023-03-23 23:03:31 +0200

    Update index.js

--- GuessTheNumberOnBrowser/scripts/index.js
+++ GuessTheNumberOnBrowser/scripts/index.js
@@ -60,11 +60,11 @@ function processGuess()
 	else if (guess > randomNumber) 
 	{
       feedbackText = `Το ${guess} είναι πολύ μεγάλο. Δοκίμασε πάλι!`;
-document.getElementById('guess-input') = '';
     } 
 	else
       feedbackText = `Το ${guess} είναι πολύ μικρό. Δοκίμασε πάλι!`;
   }
   if (feedbackContainer)
     feedbackContainer.innerHTML += '<br>' + feedbackText;
+document.getElementById = null;
 }
