4f4b187ed3210a94766c19756797752443c748cf
Delbi76 <apostolos.delbizis@gmail.com>
2023-04-19 15:43:39 +0300

    - Weeks Added

--- DaysToSummer/scripts/index.js
+++ DaysToSummer/scripts/index.js
@@ -10,12 +10,18 @@ $(function()
       minutes = Math.floor(seconds/60);
       hours = Math.floor(minutes/60);
       days = Math.floor(hours/24);
+      weeks = Math.floor(days/7);
       
-      hours = hours-(days*24);
-      minutes = minutes-(days*24*60)-(hours*60);
-      seconds = seconds-(days*24*60*60)-(hours*60*60)-(minutes*60);
+      //hours = hours-(days*24);
+      //minutes = minutes-(days*24*60)-(hours*60);
+      //seconds = seconds-(days*24*60*60)-(hours*60*60)-(minutes*60);
 
-      $("#time").html(days+" Ημέρες,<br>"+hours+" Ώρες,<br>"+minutes+" Λεπτά και<br>"+seconds+" Δευτ/λεπτα!");
+      days = days - (weeks*7);
+      hours = hours - (weeks*7*24)-(days*24);
+      minutes = minutes-(weeks*7*24*60)-(days*24*60)-(hours*60);
+      seconds = seconds-(weeks*7*24*60*60)-(days*24*60*60)-(hours*60*60)-(minutes*60);
+
+      $("#time").html(weeks+" Εβδομάδες,<br>"+days+" Ημέρες,<br>"+hours+" Ώρες,<br>"+minutes+" Λεπτά και<br>"+seconds+" Δευτ/λεπτα!");
 
       //$("#vDays").text(days+" Ημέρες");
       //$("#vHours").text(hours+" Ώρες");
